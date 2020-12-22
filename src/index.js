c953b850f63df3b19b44a570e3182598ab114933
dszarkiel <d.szarkiel@gmail.com>
2020-12-22 09:42:53 -0600

    commit tree 43c13fb92c52a0e279f92f2ebe262aa4c49c5a7c

--- src/index.js
+++ src/index.js
@@ -1,3 +1,75 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    fetchDogs();
 })
+
+const dogsAPI = "http://localhost:3000/dogs"
+const dogTable = document.querySelector("#table-body")
+const form = document.querySelector("form")
+
+function fetchDogs() {
+    fetch(dogsAPI)
+    .then(response => response.json())
+    .then(dogs => {
+        dogs.forEach(showDogs)
+    })
+}
+
+function showDogs(dog) {
+
+    let dogTr = document.createElement("tr")
+
+    let dogName = document.createElement("td")
+    dogName.textContent = dog.name 
+
+    let dogBreed = document.createElement("td")
+    dogBreed.textContent = dog.breed 
+
+    let dogSex = document.createElement("td")
+    dogSex.textContent = dog.sex 
+
+    let dogEditTag = document.createElement("td")
+    dogEditTag.dataset.id = dog.id
+    
+    let dogEditBtn = document.createElement("button")
+    dogEditBtn.textContent = "Edit"
+    dogEditTag.append(dogEditBtn)
+
+    dogEditBtn.addEventListener("click", e => {
+        e.preventDefault();
+        populateForm(dog);
+    })
+
+    dogTr.append(dogName, dogBreed, dogSex, dogEditTag)
+    dogTable.append(dogTr)
+}
+
+function populateForm(dog) {
+    form.name.value = dog.name
+    form.breed.value = dog.breed
+    form.sex.value = dog.sex
+    form.dataset.id = dog.id
+}
+
+function renderDogs() {
+    fetch(dogsAPI)
+    .then(response => response.json())
+    .then(dogs => {
+        dogs.forEach(showDogs)
+    })
+}
+
+form.addEventListener("submit", e=> {
+    let dogId = form.dataset.id
+    let dogName = form.name.value
+    let dogBreed = form.breed.value
+    let dogSex = form.sex.value
+    
+    fetch(`http://localhost:3000/dogs/${dogId}`, {
+        method: "PATCH",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({name: dogName, breed: dogBreed, sex: dogSex})
+    })
+
+    fetchDogs();
+})
+
